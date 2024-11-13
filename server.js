bf5bd57a67698852d817fb3481d09b1f49a0f184
asadbek0512 <khusanovasadbek777@gmail.com>
2024-11-13 19:43:50 +0900

    Loyiha uchun yangiliklar

--- server.js
+++ server.js
@@ -1,24 +1,28 @@
-const http = require('http'); // http modulini kiritish
-const express = require('express');
-const app = express();
-
 console.log("Web serverni boshlash");
+const express = require('express');
+const res = require("express/lib/response")
+const app = express();
+const http = require('http'); 
 
 // 1. Kirish code
 app.use(express.static("public"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// 2. Session
-
+// 2. Session code
 // 3. Views code
 app.set("views", "views");
 app.set("view engine", "ejs");
 
 // 4. Routing code
-app.get("/", function (req, res) {
-    res.end("HELLO WORLDa");
+app.post("/create-item", (req, res) => {
+    console.log(req.body);  
+    res.json({ test: "success" });
 });
+
+app.get("/", function (req, res) {
+    res.render("harid");
+})
 
 const server = http.createServer(app); 
 let PORT = 3000;
